cebcc75971885c2a5c26e4c20b939cd82560ad21
Bryan Teng <bteng1226@gmail.com>
2018-06-28 18:36:10 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,7 @@ class Hash
   def keys_of(arguments)
     animals=[]
     self.each do |x,y| 
-      puts x
-      if x == arguments
+      if x == "arguments"
         animals.push(y)
       end
     end
